c0e95ab0fcc43a3cf6ceee8b0ec7c14493ea3629
BiditG <biditgiri123@gmail.com>
2025-04-01 22:28:39 +0545

    Workbook 1-5

--- Learning_Materials/week_3/studentcode/student_wb3.py
+++ Learning_Materials/week_3/studentcode/student_wb3.py
@@ -27,16 +27,50 @@ class DepthFirstSearch(SingleMemberSearch):
         next_soln = CandidateSolution()
 
         # ====> insert your pseudo-code and code below here
+    
 
-                # my_index ← GetLastIndex(open_list)
-        # the_candidate ← open_list[my_index]
-        # RemoveFromOpenList(my_index)
-        # Return(the_candidate)
+        my_index = len(self.open_list) - 1
 
-        # ====> Actual Code:
-        my_index = len(self.open_list) - 1  
-        the_candidate = self.open_list[my_index]  
-        self.open_list.pop(my_index)  
+        the_candidate = self.open_list[my_index]
 
+    
+        self.open_list.pop(my_index)
+
+       
+        return the_candidate
+        # <==== insert your pseudo-code and code above here
+        return next_soln
+
+class BreadthFirstSearch(SingleMemberSearch):
+    """your implementation of depth first search to extend
+    the superclass SingleMemberSearch search.
+    Adds  a __str__method
+    Over-rides the method select_and_move_from_openlist
+    to implement the algorithm
+    """
+
+    def __str__(self):
+        return "breadth-first"
+
+    def select_and_move_from_openlist(self) -> CandidateSolution:
+        """Implements the breadth-first search algorithm
+
+        Returns
+        -------
+        next working candidate (solution) taken from openlist
+        """
+        # create a candidate solution variable to hold the next solution
+        next_soln = CandidateSolution()
+
+        # ====> insert your pseudo-code and code below here
+        
+        my_index = 0
+
+        
+        the_candidate = self.open_list[my_index]
+
+        self.open_list.pop(my_index)
+
+        return the_candidate
         # <==== insert your pseudo-code and code above here
         return next_soln
